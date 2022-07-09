8b7d2d377dfc5a19ee2c1f23c020c5ea2de003e9
gingercw <gingercw@gmail.com>
2022-07-09 02:24:32 +0000

    fixed address book error

--- server.py
+++ server.py
@@ -346,4 +346,4 @@ def hide_contact(contact_id):
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
     connect_to_db(app)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
